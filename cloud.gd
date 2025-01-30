51fea303f288807d1d8fb735a61fd967f888e828
Yaromeister <Yaromeister@proton.me>
2025-01-30 20:37:06 +0100

    improved moving script

--- cloud.gd
+++ cloud.gd
@@ -1,16 +1,29 @@
 extends CharacterBody2D
 
+const max_speed = 600
+const accel = 700
+const friction = 1000
 
-const SPEED = 500.0
+var input = Vector2.ZERO
 
+func _physics_process(delta):
+	player_movement(delta)
 
+func get_input():
+	input.x = int(Input.is_action_pressed("right_move")) - int(Input.is_action_pressed("left_move"))
+	return input.normalized()
 
-func _physics_process(delta): #pohyb oblacku
-
-	var direction = Input.get_axis("left_move", "right_move")
-	if direction:
-		velocity.x = direction * SPEED
+func player_movement(delta):
+	input = get_input()
+	
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(max_speed)
 
 	move_and_slide()
