ec4f96afa4d10d2048eeb26ab3538046ef025049
Tomkoid <tomaszierl@outlook.com>
2025-01-30 20:48:16 +0100

    feat: add vsync option to menu
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -3,8 +3,19 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$VsyncButton.text = check_vsync_mode(DisplayServer.window_get_vsync_mode())
+
 	pass # Replace with function body.
 
+func check_vsync_mode(mode: int) -> String:
+	if mode == DisplayServer.VSYNC_DISABLED:
+		return "VSYNC: disabled"
+	if mode == DisplayServer.VSYNC_ENABLED:
+		return "VSYNC: enabled"
+	if mode == DisplayServer.VSYNC_ADAPTIVE:
+		return "VSYNC: adaptive"
+
+	return "VSYNC: unknown"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,4 +24,18 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
+	pass # Replace with function body.
+
+
+var vsync_index = 1
+
+func _on_vsync_button_pressed() -> void:
+	vsync_index += 1
+
+	# reset vsync_index
+	if vsync_index == 3:
+		vsync_index = 0
+	
+	$VsyncButton.text = check_vsync_mode(vsync_index)
+	DisplayServer.window_set_vsync_mode(vsync_index)
 	pass # Replace with function body.
