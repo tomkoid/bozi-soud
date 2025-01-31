9e777ad83f4b0d46b7d5b2dfafc66493df9ef5e8
Yaromeister <Yaromeister@proton.me>
2025-01-31 16:09:45 +0100

    added mouse movement

--- cloud.gd
+++ cloud.gd
@@ -9,6 +9,7 @@ var input = Vector2.ZERO
 
 func _physics_process(delta):
 	player_movement(delta)
+	position.x = get_global_mouse_position().x
 
 func get_input():
 	input.x = int(Input.is_action_pressed("right_move")) - int(Input.is_action_pressed("left_move"))
