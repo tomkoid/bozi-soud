8c1413a387dbe28b2cba13a720b574ccb32fde94
Tomkoid <tomaszierl@outlook.com>
2025-01-31 21:26:44 +0100

    feat: fucking fall guy physics
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- scenes/fall_guy.gd
+++ scenes/fall_guy.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var jump_pos_x = randi_range(150,350)
+var jump_pos_x = randi_range(150,300)
 var first_jump = false
 
 func _physics_process(delta):
@@ -15,12 +15,15 @@ func _physics_process(delta):
 	# Handle jump.
 	if position.x >= jump_pos_x and is_on_floor() and not first_jump:
 		first_jump = true
+		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
 
 	velocity.x = 1 * SPEED
 	move_and_slide()
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print(body.name)
+func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
+	if body.name == "Player":
+		print("trigger")
+		velocity.y = -1500
 	pass # Replace with function body.
