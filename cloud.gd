4b2bfd902922cbce6b80c38322542456a3b2c359
Tomkoid <tomaszierl@outlook.com>
2025-01-31 17:41:11 +0100

    feat: new collision shit, random player jumping from bridge
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- cloud.gd
+++ cloud.gd
@@ -10,30 +10,31 @@ var input = Vector2.ZERO
 func _physics_process(delta):
 	player_movement(delta)
 	position.x = get_global_mouse_position().x
+	position.y = 500
 
-func get_input():
-	input.x = int(Input.is_action_pressed("right_move")) - int(Input.is_action_pressed("left_move"))
-	return input.normalized()
+#func get_input():
+	#input.x = int(Input.is_action_pressed("right_move")) - int(Input.is_action_pressed("left_move"))
+	#return input.normalized()
 
 func player_movement(delta):
-	input = get_input()
+	#input = get_input()
 	
 	
-	if input == Vector2.ZERO:
-		if velocity.length() > (friction * delta):
-			velocity -= velocity.normalized() * (friction * delta)
-		else:
-			velocity = Vector2.ZERO
-	else:
-		velocity += (input * accel * delta)
-		velocity = velocity.limit_length(max_speed)
+	#if input == Vector2.ZERO:
+		#if velocity.length() > (friction * delta):
+			#velocity -= velocity.normalized() * (friction * delta)
+		#else:
+			#velocity = Vector2.ZERO
+	#else:
+		#velocity += (input * accel * delta)
+		#velocity = velocity.limit_length(max_speed)
 
 	move_and_slide()
 
 
-func _on_area_2d_area_entered(area):
-	$AnimatedSprite2D.play("bounce")
-	$"../Enemy".velocity.y = JUMP_VELOCITY
-	position.y = 510
-	await get_tree().create_timer(0.25).timeout
-	position.y = 500
+#func _on_area_2d_area_entered(area):
+	#$AnimatedSprite2D.play("bounce")
+	#$"../FallGuy".velocity.y = JUMP_VELOCITY
+	#position.y = 510
+	#await get_tree().create_timer(0.25).timeout
+	#position.y = 500
--- scenes/fall_guy.gd
+++ scenes/fall_guy.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var jump_pos_x = randi_range(150,350)
+var first_jump = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -11,17 +13,14 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if position.x >= jump_pos_x and is_on_floor() and not first_jump:
+		first_jump = true
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	
-
+	velocity.x = 1 * SPEED
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body.name)
+	pass # Replace with function body.
