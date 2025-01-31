c218ea1cc279410526b89dbe99064b01d716013c
Tomkoid <tomaszierl@outlook.com>
2025-01-31 14:08:38 +0100

    chore: move game.tscn to scenes/
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -23,7 +23,7 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 	pass # Replace with function body.
 
 
