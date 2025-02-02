4a3e7c0b132a125a62fdf953d254b90600774453
Tomkoid <tomaszierl@outlook.com>
2025-02-02 19:12:39 +0100

    feat: add discord rpc
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- global.gd
+++ global.gd
@@ -4,7 +4,14 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	DiscordRPC.app_id = 1335673296359915610 # Application ID
+	DiscordRPC.details = "Religious game about the day of judgement."
+	DiscordRPC.state = "Playing"
+
+	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
+	# DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00:00 remaining"
+
+	DiscordRPC.refresh() # Always refresh after changing the values!
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
