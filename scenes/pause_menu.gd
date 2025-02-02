f883c1b2efb94710c62ab5c2736f863b125976a4
Yaromeister <Yaromeister@proton.me>
2025-02-02 15:48:40 +0100

    fixed bug, added restart button

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -9,9 +9,18 @@ func _on_continue_pressed():
 
 func _on_settings_pressed():
 	$".".hide()
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+	
 
 
 func _on_menu_pressed():
 	$".".hide()
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	
+
+func _on_restart_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	
