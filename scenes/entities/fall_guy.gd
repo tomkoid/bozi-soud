1f8ae25c84f44147715f8980a24c3973126d91e0
Tomkoid <tomaszierl@outlook.com>
2025-02-08 16:18:06 +0100

    chore: make bad guys fall faster than good guys
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- scenes/entities/fall_guy.gd
+++ scenes/entities/fall_guy.gd
@@ -14,9 +14,9 @@ func _ready() -> void:
 	var guy_type = get_meta("type")
 	
 	if guy_type == "good":
-		jump_velocity = randi_range(-350, -250)
-	if guy_type == "bad":
 		jump_velocity = randi_range(-500, -400)
+	if guy_type == "bad":
+		jump_velocity = randi_range(-350, -250)
 	
 	if dir == 1:
 		jump_pos_x = randi_range(50, 200)
