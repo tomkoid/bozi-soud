dc042498d2303337f19cb7ba607a98b07aa703cf
Yaromeister <Yaromeister@proton.me>
2025-02-14 21:31:30 +0100

    added transition

--- scenes/ui/menu.gd
+++ scenes/ui/menu.gd
@@ -7,6 +7,8 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	Global.game_controller.change_gui_scene("res://scenes/level.tscn")
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
 
 
 func _on_settings_button_pressed():
