b3859332e04d6cedf0dc52c297318f712311d594
grreby <grreby@proton.me>
2025-02-21 23:58:50 +0100

    feat: add slowness collectible

--- scenes/game_speed_collect.gd
+++ scenes/game_speed_collect.gd
@@ -6,9 +6,16 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		$".".visible = false # instead of calling queue_free() we make the sprite invisible
 		$Area2D/CollisionShape2D.set_deferred("disabled",true) # and disable the collision shape, otherwise the timer wouldn't work 
 		
-		%GameTimer.wait_time /= 2
-		$"../TimerLabel".text = str(%GameTimer.wait_time)
-		await get_tree().create_timer(5.0).timeout
-		%GameTimer.wait_time = StartSpawnRate
-		$"../TimerLabel".text = str(%GameTimer.wait_time)
-		queue_free()
+		
+		if self.name == "GameSpeedCollect":
+			%GameTimer.wait_time /= 2
+			await get_tree().create_timer(5.0).timeout
+			%GameTimer.wait_time = StartSpawnRate
+			queue_free()
+		if self.name == "GameSlowCollect":
+			%GameTimer.wait_time *= 2
+			await get_tree().create_timer(5.0).timeout
+			%GameTimer.wait_time = StartSpawnRate
+			queue_free()
+		else:
+			queue_free()
