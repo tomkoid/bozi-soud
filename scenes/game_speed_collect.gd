1247855afcd13ccd612d7c67ee9d1e7b4c886010
grreby <grreby@proton.me>
2025-02-22 20:55:01 +0100

    add sprites to fast and slow collectibles

--- scenes/game_speed_collect.gd
+++ scenes/game_speed_collect.gd
@@ -2,9 +2,9 @@ extends Sprite2D
 
 @onready var GameTimer = get_node("../../GameTimer")
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "FallGuy" or "BadGuy":
+		self.set_meta("started", "true")
 		var StartSpawnRate = GameTimer.wait_time
 		print(StartSpawnRate)
 		$".".visible = false # instead of calling queue_free() we make the sprite invisible
@@ -12,20 +12,17 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		
 		print(body.get_meta("collect_type"))
 		if self.get_meta("collect_type") == "fast":
-			GameTimer.wait_time /= 4
+			GameTimer.wait_time /= 2
 			print(StartSpawnRate)
 			await get_tree().create_timer(5.0).timeout
 			GameTimer.wait_time = StartSpawnRate
 			queue_free()
 			
 		if self.get_meta("collect_type") == "slow":
-			GameTimer.wait_time *= 4
+			GameTimer.wait_time *= 2
 			await get_tree().create_timer(5.0).timeout
 			GameTimer.wait_time = StartSpawnRate
 			queue_free()
 			
 		else:
 			queue_free()
-			
-func _process(_delta: float) -> void:
-	pass
