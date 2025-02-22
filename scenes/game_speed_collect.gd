affd82f63f4aac8c0fc946a10b84920eec747540
Tomkoid <tomaszierl@outlook.com>
2025-02-22 22:06:39 +0100

    feat: add working base for game speed collectibles
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- scenes/game_speed_collect.gd
+++ scenes/game_speed_collect.gd
@@ -1,16 +1,30 @@
 extends Sprite2D
 
 @onready var GameTimer = get_node("../../GameTimer")
+@onready var despawn_timer = $DespawnTimer
+
+func _ready():
+	despawn_timer.one_shot = true
+	despawn_timer.timeout.connect(_on_despawn_timer)
+	despawn_timer.start()
+
+func _on_despawn_timer():
+	print("!JK!JJL!JKLJKFHKLDHKLFHJKLDHKLDFHKLDFHKLdfjkldfhjklsh")
+	visible = false
+	$Area2D/CollisionShape2D.set_deferred("disabled",true)
+	await get_tree().create_timer(10.0).timeout
+	print("ENTITY DESPAWN")
+	queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("ARTATAFFGDSDHJSDHKDHS")
 	if body.name == "FallGuy" or "BadGuy":
 		self.set_meta("started", "true")
 		var StartSpawnRate = GameTimer.wait_time
-		print(StartSpawnRate)
 		$".".visible = false # instead of calling queue_free() we make the sprite invisible
 		$Area2D/CollisionShape2D.set_deferred("disabled",true) # and disable the collision shape, otherwise the timer wouldn't work 
-		 
-		print(body.get_meta("collect_type"))
+		#$Area2D/CollisionShape2D.queue_free() 
+		
 		var wait_timer = get_tree().create_timer(5.0)
 		if self.get_meta("collect_type") == "fast":
 			GameTimer.wait_time /= 2
