410c86576812721b14300475cebbafafdda7fa89
grreby <grreby@proton.me>
2025-04-22 18:53:39 +0200

    add collectible sound effects

--- scenes/game_collect.gd
+++ scenes/game_collect.gd
@@ -21,6 +21,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "FallGuy" or "BadGuy":
 		self.set_meta("started", "true")
 		var StartSpawnRate = GameTimer.wait_time
+		
 		$BubblePinkSprite.play("bubble_destroy")
 		$CollectAnim.visible = false # instead of calling queue_free() we make the sprite invisible
 		$Area2D/CollisionShape2D.set_deferred("disabled",true) # and disable the collision shape, otherwise the timer wouldn't work 
@@ -29,9 +30,11 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		var wait_timer = get_tree().create_timer(5.0)
 		if self.get_meta("collect_type") == "fast":
 			GameTimer.wait_time /= 2
+			$SpeedUpAudio.play()
 			
 		if self.get_meta("collect_type") == "slow":
 			GameTimer.wait_time *= 2
+			$SlowDownAudio.play()
 
 		await wait_timer.timeout
 		GameTimer.wait_time = StartSpawnRate
