c19ebfac610275282916aa4e11877b88740d5dd0
Tomkoid <tomaszierl@outlook.com>
2025-09-16 16:40:42 +0200

    chore: use tween for removing body and changing money label, stop using stats
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 var LIVES_COUNT = 3
 
-@export var stats: Stats
+var score = 0
+var lives = 0
+
+@onready var fall_guy_scene = load("res://scenes/entities/fall_guy.tscn")
+@onready var bad_guy_scene = load("res://scenes/entities/bad_guy.tscn")
 
 var save_path = "user://game.save"
 
@@ -19,9 +23,9 @@ func save():
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# reset stats
-	stats.lives = LIVES_COUNT
-	stats.score = 0
-	stats.score = 0
+	lives = LIVES_COUNT
+	score = 0
+	score = 0
 	
 	if FileAccess.file_exists(save_path):
 		var file = FileAccess.open(save_path, FileAccess.READ)
@@ -41,14 +45,14 @@ func _process(_delta: float) -> void:
 		Global.game_controller.reload_currrent_scene()
 
 
-	if stats.lives == 1:
+	if lives == 1:
 		if Global.input_method == Global.INPUT_SCHEMES.CONTROLLER and Input.get_joy_vibration_duration(0) == 0.0:
 			Input.start_joy_vibration(0, 1.0, 1.0, 5)
 
 
-	if stats.lives <= 0:
-		if game_data.best_score < stats.score or game_data.best_score == 0:
-			game_data.best_score = stats.score
+	if lives <= 0:
+		if game_data.best_score < score or game_data.best_score == 0:
+			game_data.best_score = score
 			save()
 
 
@@ -57,13 +61,13 @@ func _process(_delta: float) -> void:
 			Input.start_joy_vibration(0, 1.0, 1.0, 0.7)
 			
 		%UI.hide()
-		get_node("EndScreen/ScoreContainer/ScoreLabel").text += str(stats.score)
+		get_node("EndScreen/ScoreContainer/ScoreLabel").text += str(score)
 		get_node("EndScreen/ScoreContainer/BestScoreLabel").text += str(game_data.best_score)
 		get_node("EndScreen").show()
 		get_tree().paused = true
 
-		stats.lives = LIVES_COUNT
-		stats.score = 0
+		lives = LIVES_COUNT
+		score = 0
 	
 	if Input.is_action_just_pressed("escape"):
 		%UI.hide()
@@ -76,10 +80,6 @@ func _process(_delta: float) -> void:
 		until_reset_node.text = "next reset: " + str(roundi($GameResetFail.time_left)) + "s"
 	else:
 		until_reset_node.hide()
-
-
-var fall_guy_scene = load("res://scenes/entities/fall_guy.tscn")
-var bad_guy_scene = load("res://scenes/entities/bad_guy.tscn")
 
 func _on_game_timer_timeout() -> void:
 	var instance
@@ -123,22 +123,24 @@ func refresh_fail_count():
 	var fc_node = get_node("UI/Control/VBOX/Control/LivesCount")
 	# this function gives us the proper red color based on the lives count
 	# f(n) = 1.5 - 0.5n
-	var text_color = Color(1.5-0.5*stats.lives,0.0,0.0,1.0)
+	var text_color = Color(1.5-0.5*lives,0.0,0.0,1.0)
 	fc_node.set("theme_override_colors/default_color", text_color)
 	
 	var prefix: String
 	var suffix: String
 	
-	if stats.lives == 1:
+	if lives == 1:
 		prefix = "[shake rate=80.0 level=10 connected=1]"
 		suffix = "[/shake]"
 	
 	
-	fc_node.text = prefix + str(stats.lives) + suffix
+	fc_node.text = prefix + str(lives) + suffix
+
+var killzone_tween
 
 func killzone(body: CharacterBody2D, good_type: String):
 	if body.get_meta("type") == good_type:
-		stats.score += randi_range(100, 110)
+		score += randi_range(100, 110)
 	else:
 		# controller vibration
 		if Global.input_method == Global.INPUT_SCHEMES.CONTROLLER:
@@ -156,11 +158,18 @@ func killzone(body: CharacterBody2D, good_type: String):
 			
 		$Angel.play("cry")
 		
-		stats.lives -= 1
+		lives -= 1
 	
-	get_node("UI/Control/VBOX/Control2/MoneyLabel").text = str(stats.score)
+	var money_node = get_node("UI/Control/VBOX/Control2/MoneyLabel")
+
+	if killzone_tween and killzone_tween.is_running():
+		killzone_tween.kill()
+
+	killzone_tween = get_tree().create_tween()
+	killzone_tween.tween_property(money_node, "text", str(score), 0.6)
+	killzone_tween.tween_callback(body.queue_free)
+
 	refresh_fail_count()
-	body.queue_free()
 
 
 func _on_bottom_count_zone_body_entered(body):
@@ -170,7 +179,7 @@ func _on_top_count_zone_body_entered(body):
 	killzone(body, "good")
 
 func _on_game_reset_fail_timeout() -> void:
-	stats.lives = LIVES_COUNT
+	lives = LIVES_COUNT
 	refresh_fail_count()
 	
 
--- scenes/stats.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name Stats
-
-@export var score: int
-@export var lives: int
